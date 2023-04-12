8676288c4fb4a245c779bc19e58821ce258411ef
alireza-mehrzad <alireza.mehrzad369@gmail.com>
2023-04-12 11:52:25 -0700

    get props

--- pages/users.js
+++ pages/users.js
@@ -1,5 +1,22 @@
-const userList = () => {
-  return <h1>userList</h1>;
+const userList = ({ users }) => {
+  return (
+    <>
+      <h1>userList</h1>;
+      
+    </>
+  );
 };
 
 export default userList;
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  console.log(data);
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+}
